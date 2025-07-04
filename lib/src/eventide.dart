fc86f83d3cc2bcf11814a7424af74229572cb0a1
Alexis Choupault <alexis_choupault@connect-tech.sncf>
2025-07-04 12:25:29 +0200

    tst flutter

--- lib/src/eventide.dart
+++ lib/src/eventide.dart
@@ -10,6 +10,8 @@ import 'package:eventide/src/extensions/color_extensions.dart';
 import 'package:eventide/src/extensions/duration_extensions.dart';
 import 'package:eventide/src/extensions/event_extensions.dart';
 
+class ToBeRemoved {}
+
 class Eventide extends EventidePlatform {
   final CalendarApi _calendarApi;
 
